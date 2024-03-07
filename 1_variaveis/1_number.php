1958401f308c2843379619026ebe3bda6cb3b0ee
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2024-03-06 22:14:35 -0300

    Add files via upload

---
+++ 1_variaveis/1_number.php
@@ -0,0 +1,10 @@
+<?php
+/* variaveis */
+$idade = 17; //inteiro//
+$nome = RODRIGO XABIER; //string//
+$estuda = null; // boleano //
+$salario = 0.00;
+echo 'Olá me chamo' . $nome. 'e tenho '. $idade .;
+
+echo  'Estudo: ' . $estuda . 'Salário' . $salario;
+?>
