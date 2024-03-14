8f39ca7c7af3d4463816db30501f1ff20f1aff3a
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2024-03-13 22:08:53 -0300

    Add files via upload

---
+++ 1_variaveis/2_operador.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = 24;
+$b = 16;
+
+$c= $a + $b;
+echo '<br>A soma é ' . $c;
+
+$c= $a * $b;
+echo '<br>A soma é ' . $c;
+
+$c= $a - $b;
+echo '<br>A soma é ' . $c;
+
+$c= $a / $b;
+echo '<br>A soma é ' . $c;
+?>
