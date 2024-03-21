98807791022a2398e07c28c47f0b18899c48371c
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2024-03-20 22:16:26 -0300

    Add files via upload

---
+++ 2_operadores_logicos/2_if_else.php
@@ -0,0 +1,9 @@
+<?php
+$idade = 20;
+
+if($idade < 18){
+    echo 'Você não pode entrar aqui!';
+} else{
+    echo 'Seja bem vindo - TOTOSA';
+}
+?>
---
+++ 2_operadores_logicos/3_logico_operadores.php
@@ -0,0 +1,10 @@
+<?php
+$idade = 21;
+$identidade = false;
+if($idade >18 && $identidade == true){
+    echo 'seja bem vindo';
+}else{
+    echo 'VALEU FALOU';
+
+    }
+?>
