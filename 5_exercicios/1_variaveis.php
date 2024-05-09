e48773ee49cb15a198452116af397af57ab2659e
RODRIGOSUNSHINY <125514493+RODRIGOSUNSHINY@users.noreply.github.com>
2024-05-08 22:20:20 -0300

    Add files via upload

---
+++ 5_exercicios/1_variaveis.php
@@ -0,0 +1,15 @@
+a) crie o cadastro do cliente e armazene 
+os seguintes dados em variaveis.
+Nome, idade, endereço, e-mail
+
+
+b) verifique se cada cliente é de idade maior. se for
+maior que 17 anos escreva "O cliente 'NOME' é de maior";
+<php 
+$nome = "neta"
+$idade = "39"
+$indereço = " ESQUERDA"
+
+
+?>
+    
